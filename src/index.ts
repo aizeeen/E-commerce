816442e382ad3a5ea9b9b29a3de8c7bed139180c
aizeeen <ihebbacha2006@gmail.com>
2024-10-17 12:41:29 +0100

    user router endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
 
 const app = express();
 const port = 3001;
+
+app.use(express.json());
 
 mongoose
   .connect(
@@ -10,6 +13,8 @@ mongoose
   )
   .then(() => console.log("mongo connected"))
   .catch ((err) => console.log("failed to connect", err));
+
+app.use('/user', userRoute)
 
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
