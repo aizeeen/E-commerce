b7788f846c8b1f37f974244d20dd92a65967b91a
aizeeen <ihebbacha2006@gmail.com>
2024-10-19 23:12:37 +0100

    cart schema

---
+++ src/models/cartModel.ts
@@ -0,0 +1,34 @@
+import mongoose , { Schema, Document, ObjectId } from "mongoose"
+import { IProduct } from "./productModel";
+
+const CartStatusEnum = ["Active", "completed"]
+
+export interface ICartItem extends Document{
+    product: IProduct
+    unitPrice: number
+    quantity: number;
+}
+
+
+export interface ICart extends Document {
+    userId: ObjectId | string;
+    items: ICartItem []
+    totalAmount: number
+    status : "active" | "completed"
+}
+
+const cartItemSchema = new Schema<ICartItem>({
+    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    unitPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true, default: 1},
+
+});
+
+const cartSchema = new Schema<ICart>({
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true},
+    items: [cartItemSchema],
+    totalAmount: { type: Number, required: true, default: 0},
+    status: { type: String, enum: CartStatusEnum, default: "active" },
+    })
+
+    export const cartModel = mongoose.model<ICart>("cart", cartSchema)
