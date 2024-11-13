20772e3d8ce7400afc0f2ada83fdcf2078458154
aizeeen <ihebbacha2006@gmail.com>
2024-11-13 23:02:19 +0100

    cart pages

--- Frontend/src/pages/CartPages.tsx
+++ Frontend/src/pages/CartPages.tsx
@@ -1,46 +1,52 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
 //import { useEffect, useState } from "react";
 //import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import { useCart } from "../context/Cart/CartContext";
-import { useState } from "react";
 
 const CartPage = () => {
-  const { token } = useAuth();
-const { cartItems, totalAmount} = useCart()
-  const [error, setError] = useState<string | undefined>(undefined);
+  const { cartItems, totalAmount } = useCart();
 
- /* useEffect(() => {
-    if (!token) {
-      return;
-    }
-
-    const fetchCart = async () => {
-      const response = await fetch(`${BASE_URL}/cart`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        setError("failed to fetch user cart. please try again");
-      }
-
-      const data = await response.json();
-      setCart(data);
-    };
-    fetchCart();
-  }, [token]);
-
-  console.log({ cart });
-  */
   return (
     <Container sx={{ mt: 2 }}>
       <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) => (
-        <div key={item.title}></div> ))}  
+      <Box display="flex" flexDirection="column" gap={4}>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              border: 1,
+              borderColor: "#f2f2f2",
+              borderRadius: 5,
+              padding: 2,
+            }}
+          >
+            <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+              <img src={item.image} width={150}/>
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} x {item.unitPrice} DINAR
+                </Typography>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" arial-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+            <Button>Remove item</Button>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant="h4">Total Amount: {totalAmount.toFixed(2)} DINAR</Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
-
 export default CartPage;
